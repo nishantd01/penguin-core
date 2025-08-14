189a76a0bfc697461ceb68e00e62a03a93d2d75d
Kshitij Mathur <k.mathur68@gmail.com>
2025-08-14 16:03:57 +0530

    first version

--- main.go
+++ main.go
@@ -1,23 +1,53 @@
 package main
 
 import (
-	"github.com/nishantd01/penguin-core/service"
-
-	v1 "github.com/nishantd01/penguin-core/controllers/v1"
+	"database/sql"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	v1 "github.com/nishantd01/penguin-core/controllers/v1"
+	"github.com/nishantd01/penguin-core/service"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = ""
+	password = ""
+	dbname   = ""
 )
 
 func main() {
-	r := gin.Default()
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-	userService := service.NewUserService()
-	userController := v1.NewUserController(userService)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	v1Group := r.Group("/v1")
-	{
-		v1Group.GET("/users/:id", userController.GetUser)
+	err = db.Ping()
+	if err != nil {
+		panic(err)
 	}
 
-	r.Run(":8080")
+	fmt.Println("Successfully connected")
+
+	r := gin.Default()
+
+	userService := service.NewUserService(db)
+	userController := v1.NewUserController(userService)
+
+	v1Group := r.Group("/api/v1")
+	{
+		v1Group.GET("/users/:id", userController.GetUser)
+		v1Group.GET("/dbnames", userController.GetDbNames)
+		v1Group.GET("/roles", userController.GetRoles)
+		v1Group.POST("/check-edit-permission", userController.CheckAccess)
+	}
+
+	r.Run(":8083")
 }
